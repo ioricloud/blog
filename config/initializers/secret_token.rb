b0a5e8333f4a1207153808cb6ad0234cb5b8180e
henrique oliveira <ioricloud@gmail.com>
2012-01-28 09:35:31 -0300

    esborço de blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1ad334d93c828bafa00ca20e055801723706f3e9a31d7cf38fe78f9aa3aec67095ba8748e052985cdaf693dcf61907147f7ea5e91114041125a2225207e60939'
